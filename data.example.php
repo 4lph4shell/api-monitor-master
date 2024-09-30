147316a7313778d3efd0bb4a4d054ab94fadc63c
4lph4shell <4lph4ir@gmail.com>
2024-09-30 18:14:37 +0330

    add nother files

---
+++ data.example.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file seed the database with the data
+ * the data structure :
+ *  'app' => 'app-name'
+ *  'name' => 'endpoint-name'
+ *  'url' => 'test-url'
+*/
+return [
+    [
+        'app' => 'github',
+        'name' => 'api-monitor-repo',
+        'url' => 'https://github.com/alphashell/api-monitor',
+    ],
+];
