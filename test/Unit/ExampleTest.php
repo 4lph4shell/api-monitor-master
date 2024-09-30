2d41dcef51ad5ed87622d8f338fb31c46a85e5dd
4lph4shell <4lph4ir@gmail.com>
2024-09-30 18:11:20 +0330

    add tests

---
+++ test/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * Test that the true is true.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        // We're testing that the true is equal to true
+        $this->assertTrue(true);
+    }
+}
